cf6f9638a3d4abc0697d1702394d58bb882bf358
xvyabin <792134478@qq.com>
2018-11-29 15:11:00 +0800

    修改2

--- mein.js
+++ mein.js
@@ -1,36 +1,136 @@
-var div =document.getElementById('canvas')
+var yyy = document.getElementById('xxx');
+var context = yyy.getContext('2d');
 
-//按下去鼠标
-div.onmousedown =function(a){
-  var x=a.clientx
-  var y=a.clienty
-  var divA =document.createElement('div')
-  divA.style ="width:6px;height:6px;" + 
-  "background:black; border-radius:3px;" +     
-  "position: absolute; left: "+(x-3)+"px;"+
-  "top: "+(y-3)+"px;"
- div.appendChild(divA)
-   
- }
+autoSetCanvasSize(yyy)
 
-//动鼠标
-div.onmousemove =function(a){
-    var x=a.clientx
-  var y=a.clienty
-  var divA =document.createElement('div')
-  divA.style ="width:6px;height:6px;" + 
-  "background:black; border-radius:3px;" +     
-  "position: absolute; left: "+(x-3)+"px;"+
-  "top: "+(y-3)+"px;"
- div.appendChild(divA)
+listenToUser(yyy)
+
+
+var eraserEnabled = false
+eraser.onclick = function() {
+  eraserEnabled =true
+  actions.className = 'actions x'
   
 }
-//松开鼠标
-div.onmouseup =function(z){
-  console.log(1)
+brush.onclick = function(){
+  eraserEnabled = false
+  actions.className = 'actions'
 }
-var yyy = document.getElementById('xxx')
-var context = yyy.getContext('2d')
 
-context.fillStyle = 'red';
-context.fillRect(0,0,100,100);
+
+/******/
+
+function autoSetCanvasSize(canvas) {
+  setCanvasSize()
+
+  window.onresize = function() {
+    setCanvasSize()
+  }
+
+  function setCanvasSize() {
+    var pageWidth = document.documentElement.clientWidth
+    var pageHeight = document.documentElement.clientHeight
+
+    canvas.width = pageWidth
+    canvas.height = pageHeight
+  }
+}
+
+function drawCircle(x, y, radius) {
+  context.beginPath()
+  context.fillStyle = 'black'
+  context.arc(x, y, radius, 0, Math.PI * 2);
+  context.fill()
+}
+
+function drawLine(x1, y1, x2, y2) {
+  context.beginPath();
+  context.strokeStyle = 'black'
+  context.moveTo(x1, y1) // 起点
+  context.lineWidth = 5
+  context.lineTo(x2, y2) // 终点
+  context.stroke()
+  context.closePath()
+}
+
+function listenToUser(canvas) {
+  var using = false
+  var lastPoint = {
+    x: undefined,
+    y: undefined
+  }
+  if (document.body.ontouchstart !== undefined){
+     canvas.ontouchstart = function(aaa){
+      var x = aaa.touches[0].clientX
+      var y = aaa.touches[0].clientY
+      using = true
+      if (eraserEnabled) {
+      context.clearRect(x - 5, y - 5, 10, 10)
+    } else {
+      lastPoint = {
+        "x": x,
+        "y": y
+      }
+    }
+  }
+
+  canvas.ontouchmove = function(aaa){
+    var x = aaa.touches[0].clientX
+    var y = aaa.touches[0].clientY
+
+    if (!using) {return}
+
+    if (eraserEnabled) {
+      context.clearRect(x - 5, y - 5, 10, 10)
+    } else {
+      var newPoint = {
+        "x": x,
+        "y": y
+      }
+      drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+      lastPoint = newPoint
+    }
+  }
+  canvas.ontouchend = function(aaa){
+    using = true
+  }
+}else{
+  canvas.onmousedown = function(aaa) {
+    var x = aaa.clientX
+    var y = aaa.clientY
+    using = true
+    if (eraserEnabled) {
+      context.clearRect(x - 5, y - 5, 10, 10)
+    } else {
+      lastPoint = {
+        "x": x,
+        "y": y
+      }
+    }
+  }
+  canvas.onmousemove = function(aaa) {
+    var x = aaa.clientX
+    var y = aaa.clientY
+
+    if (!using) {return}
+
+    if (eraserEnabled) {
+      context.clearRect(x - 5, y - 5, 10, 10)
+    } else {
+      var newPoint = {
+        "x": x,
+        "y": y
+      }
+      drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+      lastPoint = newPoint
+    }
+
+  }
+  canvas.onmouseup = function(aaa) {
+    using = false
+  }
+}
+  
+ 
+}
+
